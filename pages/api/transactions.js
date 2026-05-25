788ecac343cd9107db5f4023ff2ef2033c7644d5
lesliezhili <zhili@phledger.com>
2026-05-25 16:47:48 +1000

    feat(api): transactions endpoint

---
+++ pages/api/transactions.js
@@ -0,0 +1,8 @@
+import { loadAll, loadBank, loadCountry } from '../../../lib/store.js';
+import { autoCategorise } from '../../../lib/categoriser.js';
+export default function handler(req, res) {
+  const { bank, country, limit = '1000' } = req.query;
+  let txs = bank ? loadBank(bank) : country ? loadCountry(country) : loadAll();
+  txs = autoCategorise(txs);
+  res.json(txs.slice(0, parseInt(limit)));
+}
